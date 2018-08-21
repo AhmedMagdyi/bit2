e90fa1a35e5b094b4d5ce2a1eab5d197341e3848
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-21 09:13:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,15 +93,6 @@ ${thisMessage}\`\`\`
 
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const fs = require('fs');
-
-client.on('ready', async () => {
-    console.log('I am ready!');
-
-    client.user.setGame("Alpha Codes.", "https://www.twitch.tv/idk");
-});
 
 
 
