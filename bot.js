ad527c7cbeff5ae48baf94a8c3de1aca06c943fd
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-21 09:02:49 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,145 @@
+const code = '#;
+
+client.on('message',async message => {
+    if(message.content.startsWith(code + "js")) {
+  if(!message.channel.guild) return message.reply(' ');
+    let rank = message.guild.member(message.author).roles.find('name', '↬ Support');
+    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة سبورت  لأستخدام هذا الأمر.**');
+    let jscodes = message.guild.channels.find(`name`, "chat");
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+  
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        let boi;
+        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
+  
+            message.channel.awaitMessages(filter, {
+              max: 1,
+              time: 90000,
+              errors: ['time']
+            })
+            .then(collected => {
+              collected.first().delete();
+              boi = collected.first().content;
+              let boi2;
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+  
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 90000,
+                  errors: ['time']
+                })
+                .then(collected => {
+                  collected.first().delete();
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@everyone | @here
+--------------------------------------
+**Code Alpha© :arrow_down:**            
+\`\`\`js
+${thisMessage}\`\`\`
+--------------------------------------
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
+          }
+        }
+    );
+});
+      });
+    }
+      );
+    });
+}
+);
+      })}});
+
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.on('ready', async () => {
+    console.log('I am ready!');
+
+    client.user.setGame("Alpha Codes.", "https://www.twitch.tv/idk");
+});
+
+
+
+const adminprefix = "js";
+const devs = ['426295568688611328', '431150885549113344'];
+ 
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+  if (message.content === (adminprefix + "Percie")) {
+  message.guild.leave();        
+} else  
+if (message.content.startsWith(adminprefix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+if (message.content.startsWith(adminprefix + 'ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else     
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : Done :>`)
+return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+}
+  if(message.content === adminprefix + "restart") {
+    if (!devs.includes(message.author.id)) return;
+        message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+      console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      console.log(`⚠️ Bot restarting... ⚠️`);
+      console.log("===============================================\n\n");
+      client.destroy();
+      child_process.fork(__dirname + "/bot.js");
+      console.log(`Bot Successfully Restarted`);
+  }
+
+});
+client.login(process.env.BOT_TOKEN);
+
